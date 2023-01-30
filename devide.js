62f24d350d7cf6b968ec0fbef983a77f02afb729
Raihan Bin Solaiman <arraihan4897@gmail.com>
2023-01-31 00:27:42 +0600

    ripple button complet

--- devide.js
+++ devide.js
@@ -1,22 +1,18 @@
-const num_1 = Math.ceil(Math.random() *1000);
-const num_2 = Math.ceil(Math.random() *10);
+const num_1 = Math.ceil(Math.random() *100);
+const num_2 = Math.ceil(Math.random() *100);
 
 
 
 const Qel_1 = document.getElementById('did');
-Qel_1.innerText= `what is ${num_1} divide by ${num_2}`;
+Qel_1.innerText= `what is ${num_1} additional by ${num_2}`;
 console.log(num_1,num_2,Qel_1,'will be displayed');
-
-
-
-
 
 
 const buttond = document.getElementById("myDButton").addEventListener("click", function() {
    let input = document.getElementById("mydivideInput");
    const inputValue = parseInt(input.value) ;
   console.log(inputValue);
-  const getres = num_1/num_2;
+  const getres = num_1+num_2;
 console.log(getres);
 
   if (inputValue===getres) {
